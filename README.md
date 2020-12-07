618eb79dbc2d6c1eca70394fefe7634e1af214ae
csh928 <csh928@gmail.com>
2020-12-07 14:00:03 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# boilerplate-mern-stack-movie-app
+ 
---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+export const API_KEY = "983206480a2c6416e06b0e2a6fc86d11";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
